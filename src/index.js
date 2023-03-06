29f40919f3054a65345be396e10694c93893798e
Korben-Dallas-React <borozan.eugen@gmail.com>
2023-03-06 07:42:35 +0200

    testimonial-section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
+import Swiper from "swiper";
+
+const swiper = new Swiper(".testimonial-slider", {
+  // Optional parameters
+  slidesPerView: 1,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
 let tl = gsap.timeline();
 
-tl.from(".card:first-child", { opacity: 0, y: 70, duration: 0.5 });
+tl.from(".card:first-child", {
+  ScrollTrigger: ".card",
+  opacity: 0,
+  y: 70,
+  duration: 0.5,
+});
 tl.from(".card:last-child", { opacity: 0, y: 70, duration: 0.5 }, "=+.1");
 tl.from(".message", { scale: 0, y: 100, duration: 0.5 }, "=+.1");
 tl.from(".gmail", { scale: 0, x: 100, duration: 0.5 }, "=+.1");
+
+ScrollTrigger.create({
+  animation: tl,
+  trigger: ".compliant",
+  start: "top top",
+  end: "bottom bottom",
+});
