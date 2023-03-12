76fc6628030ad83914febbbdc4d8828e6a3666db
Borozan Evghenii <borozan.eugen@gmail.com>
2023-03-12 09:10:08 +0200

    footer

--- src/section/testimonial/testimonial.js
+++ src/section/testimonial/testimonial.js
@@ -1,10 +1,26 @@
-import { Swiper, Navigation } from "swiper";
+import { Swiper } from "swiper";
 
 const swiper = new Swiper(".slider", {
   loop: true,
   slidesPerView: 1,
+  centeredSlides: true,
   navigation: {
     nextEl: ".button-next",
     prevEl: ".button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
   },
 });
